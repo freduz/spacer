ebdfad941bfc4d2a8b215730d8e39f4e7547c6a8
Fredy Daniel <fredyd900@gmail.com>
2023-06-14 17:31:14 +0200

    front end related works done with rxjs and ngx toaster

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,18 +1,42 @@
-import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { environment } from '../environments/environment';
+import { AlienService } from './services/alien.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   nickname: string;
 
-  data$: Observable<any>;
+  data$!: Observable<any>;
+  @ViewChild('chatInput') chatInput!: ElementRef;
 
-  constructor() {
+  constructor(
+    private alienService: AlienService,
+    private toaster: ToastrService
+  ) {
     this.nickname = environment.nickname;
+  }
+
+  ngOnInit(): void {
+    this.data$ = this.alienService.getData();
+  }
+
+  handleMessage(event: any) {
+    const message = event.target.value;
+    const payload = { nickname: this.nickname, message };
+    this.alienService.sendData(payload).subscribe({
+      next: (res) => {
+        this.data$ = of(res);
+        this.chatInput.nativeElement.value = '';
+      },
+      error: (err) => {
+        this.toaster.error('Internal server error');
+      },
+    });
   }
 }
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   api: {
-    baseUrl: 'http://localhost:1337'
+    baseUrl: 'http://localhost:1337',
+    key: 'd3d8bcbd2796ad09082bbd70cf44e786',
   },
-  nickname: 'Major Foobar'
+  nickname: 'Major Foobar',
 };
